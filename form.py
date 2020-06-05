5734eb4d5a37a9722e15446703b2ab6527b29221
azimov07 <61144833+azimov07@users.noreply.github.com>
2020-06-05 15:07:32 +0300

    Update form.py

--- form.py
+++ form.py
@@ -1,7 +1,8 @@
+
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, DateField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-from datetime import date
 
 
 class LoginForm(FlaskForm):
@@ -9,27 +10,36 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+class GiveMedicine(FlaskForm):
+    patient = StringField('Patient',validators=[DataRequired()])
+    medicine = StringField('Medicine',validators=[DataRequired()])
+    give = SubmitField('Give')
+
 class RoomForm(FlaskForm):
     roomID = IntegerField('Room_ID',validators=[DataRequired()])
     patient = StringField('Patient',validators=[DataRequired()])
     nurse = StringField('Nurse',validators=[DataRequired()])
     update = SubmitField('Update')
-    
 class MeetingForm(FlaskForm):
     description = StringField('Description',validators=[DataRequired()])
-    appointment = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()], default=date.today)
+    appointment = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     add = SubmitField('Add')
-    
 class MeetingUpdateForm(FlaskForm):
     recordID = IntegerField('Record ID',validators=[DataRequired()])
-    appointmentupd = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()], default=date.today)
+    appointmentupd = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     upd = SubmitField('Update')
-    
 class MeetingDeleteForm(FlaskForm):
     recordIDdel = IntegerField('Record ID',validators=[DataRequired()])
     dele = SubmitField('Delete')
-    
+
+
 class AddDiagnosis(FlaskForm):
     diag = SelectField("Choose Diagnosis", validators=[DataRequired()], choices=[])
     to_patient = SelectField("To Which Patient", validators=[DataRequired()], choices=[])
     upd_2 = SubmitField('Update')
+
+class D_MeetingForm(FlaskForm):
+    addPatient = IntegerField('Patient ID',validators=[DataRequired()])
+    appointment = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    add = SubmitField('Add')
